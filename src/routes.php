3f1fd23690e92fbd56c5509f9da3380d8cb93305
enrico <enrico.paravano@allievi.bearzi.it>
2021-03-18 19:17:59 +0100

    rotta

--- src/routes.php
+++ src/routes.php
@@ -35,7 +35,7 @@ switch ($action){
         setPassword();
         break;
     case 'image':
-        require('../src/controller/addImage.php');
+        require('../src/controller/Image.php');
         printImg();
         break;
 }
