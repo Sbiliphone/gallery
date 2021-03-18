ccccd029c1e80e5860ce2652264258eee9d952ca
enrico <enrico.paravano@allievi.bearzi.it>
2021-03-18 18:29:17 +0100

    prova home page

--- templates/homepage.php
+++ templates/homepage.php
@@ -6,6 +6,24 @@ require('../templates/header.php');
 require('../templates/menu.php');
 ?>
 
+
+<?php
+$files = scandir('uploades/');
+
+foreach($files as $file) {
+
+    $url ='uploades/'.$file;
+    if($url ==='uploades/.' || $url ==='uploades/..'   ){
+
+    }else{
+        ?>
+        <img src="<?php echo$url; ?>" style="height: 100px; width: 200px">
+        <?php
+    }
+}
+?>
+
+
 <?php
 require('../templates/stampaImg.php');
 ?>
