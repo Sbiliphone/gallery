9c81a98151541474e46ca46b510d627ae22b040a
enrico <enrico.paravano@allievi.bearzi.it>
2021-03-19 16:16:58 +0100

    prova home page

--- templates/homepage.php
+++ templates/homepage.php
@@ -8,18 +8,23 @@ require('../templates/menu.php');
 global $db;
 $sql = "SELECT * FROM immagine ;";
 $rs = $db->execute($sql);
-
-foreach ($rs as $result) {
-
-
 ?>
-    <div style="width: 300px; height: 300px; padding-left: 15%">
-        <h4><?php echo $result['titolo'] ?></h4>
-        <img src="<?php  echo "./uploades/".$result['src'] ?>" style="height: 100px; width: 200px">
-    </div>
-        <?php
-}
 
+    <div style="width: 100%; height: 100%;overflow: auto;">
+        <?php
+        foreach ($rs as $result) {?>
+            <div style="width: 300px; height: 300px; padding-left: 15%;display: inline-block;">
+                <h4><?php echo $result['titolo'] ?></h4>
+                <img src="<?php  echo "./uploades/".$result['src'] ?>" style="height: 100px; width: 200px">
+            </div>
+            <?php
+        }?>
+    </div>
+
+<?php
 require('../templates/footer.php');
 ?>
 
+
+
+border: solid 4px aqua;
