8577fa0413a70390668e8ddb4d3e9012bfcefadf
ale <alessandro.ligugnana@allievi.com>
2021-03-19 21:41:24 +0100

    prova

--- src/controller/editImg.php
+++ src/controller/editImg.php
@@ -1,1 +1,17 @@
 <?php
+require('../templates/header.php');
+require('../templates/menu.php');
+
+?>
+<div style="display: none">
+    <!-- query -->
+</div>
+
+<form name="user" action="index.php?action=updateUser" method="post">
+    <div class="row">
+        <div class="col-md-6 mb-3">
+            <div class="form-group"><label for="titolo" class="required">Titolo</label><input type="text" id="titolo" name="titolo" required="required" maxlength="180" class="form-control" readonly value="<?php echo ""?>"></div>
+        </div>
+        <button class="btn btn-primary">Salva</button>
+    </div>
+
