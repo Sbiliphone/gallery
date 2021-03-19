36a451fcc8cec87748871de51958f606086836b1
enrico <enrico.paravano@allievi.bearzi.it>
2021-03-19 16:05:59 +0100

    prova home page

--- templates/homepage.php
+++ templates/homepage.php
@@ -4,9 +4,26 @@ require('../templates/header.php');
 
 <?php
 require('../templates/menu.php');
-?>
 
-<?php
+global $db;
+$sql = "SELECT * FROM immagine ;";
+$rs = $db->execute($sql);
+
+foreach ($rs as $result) {
+
+
+?>
+<center>
+    <div style="width: 300px; height: 300px">
+        <h4><?php echo $result['titolo'] ?></h4>
+
+        <img src="<?php  echo "./uploades/".$result['src'] ?>" style="height: 100px; width: 200px">
+
+        <br>
+
+        <?php
+}
+
 require('../templates/footer.php');
 ?>
 
