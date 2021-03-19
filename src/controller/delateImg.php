4c8ea4f8c32e7c1e9e003fc40d8a578f51f89ca2
enrico <enrico.paravano@allievi.bearzi.it>
2021-03-19 21:18:43 +0100

    nascodno quiri

--- src/controller/delateImg.php
+++ src/controller/delateImg.php
@@ -1,8 +1,11 @@
 <?php
 $id=$_SESSION['idImmagine'];
 
+echo $id;
+
 global $db;
-$sql="DELETE FROM immagini WHERE id='$id';";
+$sql="DELETE FROM immagine WHERE id='$id';";
 $rs = $db->execute($sql);
+
 header('Location: index.php?action=image');
 ?><?php
