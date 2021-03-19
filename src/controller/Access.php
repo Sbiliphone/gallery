d38367320e1bbafb3c083869e70f563ee3e4131e
enrico <enrico.paravano@allievi.bearzi.it>
2021-03-19 11:22:57 +0100

    modifica Accesso origni

--- src/controller/Access.php
+++ src/controller/Access.php
@@ -7,21 +7,13 @@ function login()
 function loginCheck()
 {
     global $db;
-    global $db2;
-
     $username=$_REQUEST['username'];
     $password=md5($_REQUEST['password']);
     $sql="SELECT * FROM app_user WHERE username='$username' AND password='$password'";
-
-    $sql2 = "SELECT id FROM add_user WEHRE username = '$username' AND password == '$password';";
-
-    $rs2 = $db2->execute($sql2);
-
     $rs = $db->execute($sql);
     if (count($rs->getRows())>0){
         $_SESSION['authorized']=true;
         $_SESSION['username']=$username;
-        $_SESSION['id']= $rs2;
         header('Location: index.php');
     } else {
         $_SESSION['messaggio']='Credenziali non valide';
