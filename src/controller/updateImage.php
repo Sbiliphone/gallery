37a4bbf20c4a1dfb54716896d7efc3e4630fe74c
ale <alessandro.ligugnana@allievi.com>
2021-03-19 22:49:50 +0100

    prova

--- src/controller/updateImage.php
+++ src/controller/updateImage.php
@@ -1,8 +1,9 @@
 <?php
 global $db;
 $titolo=$_REQUEST['titolo'];
+$id=$_REQUEST['id'];
 
-$sql="UPDATE immagine SET titolo= '$titolo';";
+$sql="UPDATE immagine SET titolo= '$titolo' WHERE id='$id';";
 $rs = $db->execute($sql);
 
 header('Location:index.php?action=image');
