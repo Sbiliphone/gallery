3ed17c79c6b750386df09b41dd4fffbd2ab084cb
ale <alessandro.ligugnana@allievi.com>
2021-03-19 22:07:23 +0100

    prova

--- src/controller/modifyImage.php
+++ src/controller/modifyImage.php
@@ -1,16 +1,20 @@
 <?php
 require('../templates/header.php');
 require('../templates/menu.php');
-
+$id=$_REQUEST['id'];
 ?>
 <div style="display: none">
-    <!-- query -->
+    <?php
+    global $db;
+    $sql="SELECT titolo FROM image WHERE id='$id';";
+    $rs = $db->execute($sql);
+    ?>
 </div>
 
 <form name="user" action="index.php?action=updateImage" method="post">
     <div class="row">
         <div class="col-md-6 mb-3">
-            <div class="form-group"><label for="titolo" class="required">Titolo</label><input type="text" id="titolo" name="titolo"  maxlength="180" class="form-control" readonly value="<?php echo ""?>"></div>
+            <div class="form-group"><label for="titolo" class="required">Titolo</label><input type="text" id="titolo" name="titolo" required="required" maxlength="180" class="form-control" readonly value="<?php echo $rs;?>"></div>
         </div>
         <button class="btn btn-primary">Salva</button>
     </div>
