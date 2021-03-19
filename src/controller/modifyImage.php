51892b072af1787cf3e1abe347ed1a818ed49272
ale <alessandro.ligugnana@allievi.com>
2021-03-19 22:09:22 +0100

    prova

--- src/controller/modifyImage.php
+++ src/controller/modifyImage.php
@@ -14,7 +14,7 @@ $id=$_REQUEST['id'];
 <form name="user" action="index.php?action=updateImage" method="post">
     <div class="row">
         <div class="col-md-6 mb-3">
-            <div class="form-group"><label for="titolo" class="required">Titolo</label><input type="text" id="titolo" name="titolo" required="required" maxlength="180" class="form-control" readonly value="<?php echo $rs;?>"></div>
+            <div class="form-group"><label for="titolo" class="required">Titolo</label><input type="text" id="titolo" name="titolo" required="required" maxlength="180" class="form-control" value="<?php echo $rs;?>"></div>
         </div>
         <button class="btn btn-primary">Salva</button>
     </div>
