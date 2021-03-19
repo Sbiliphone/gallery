36b009728cb4dfc3557ea35bab02b4af32ca9f2c
enrico <enrico.paravano@allievi.bearzi.it>
2021-03-19 16:08:17 +0100

    prova home page

--- templates/homepage.php
+++ templates/homepage.php
@@ -13,14 +13,10 @@ foreach ($rs as $result) {
 
 
 ?>
-<center>
-    <div style="width: 300px; height: 300px">
+    <div style="width: 300px; height: 300px; padding-left: 15%">
         <h4><?php echo $result['titolo'] ?></h4>
-
         <img src="<?php  echo "./uploades/".$result['src'] ?>" style="height: 100px; width: 200px">
-
-        <br>
-
+    </div>
         <?php
 }
 
